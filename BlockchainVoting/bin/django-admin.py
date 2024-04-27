375bbd6de7965e8f90f26f7815343913899f0cfd
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2024-04-27 13:19:07 +0500

    Deleted Files

--- BlockchainVoting/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Volumes/Data/Work/DB Lab Project/Blockchain-Voting-System/BlockchainVoting/bin/python3.12
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
